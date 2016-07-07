6537bebef4634515cd7c9765fc05ebdb54b20df5
Roger Wojciech Włosek <rwl@DIFI.no>
2016-07-07 16:55:55 +0200

    ...

--- modules/indicators/client/config/indicators.client.routes.js
+++ modules/indicators/client/config/indicators.client.routes.js
@@ -30,7 +30,7 @@
             })
             .state('indicators.create',
             {
-                url: '/create?keywords',
+                url: '/create',
                 views:
                 {
                     "":
@@ -43,30 +43,11 @@
                     {
                         templateUrl: "modules/indicators/client/views/creator-side.client.view.html"
                     },
-                    "main@indicators.create":
-                    {
-                        templateUrl: "modules/indicators/client/views/indicator-form-main.client.view.html"
-                    },
-                     "activity-details@indicators.create":
-                    {
-                        templateUrl: "modules/indicators/client/views/activity-details.client.view.html"
-                    },
-                     "input-details-main@indicators.create":
-                    {
-                        templateUrl: "modules/indicators/client/views/input-details-main.client.view.html"
-                    },
-                     "activity-form@indicators.create":
-                    {
-                        templateUrl: "modules/indicators/client/views/activity-form.client.view.html"
-                    },
-                     "input-form@indicators.create":
-                    {
-                        templateUrl: "modules/indicators/client/views/input-form.client.view.html"
-                    }
                 },
                 resolve:
                 {
-                    indicatorResolve: newIndicator
+                    indicatorResolve: newIndicator,
+                    indicatorId: getIndicatorId
                 },
                 data:
                 {
@@ -74,15 +55,86 @@
                     pageTitle: 'Indicators Create'
                 }
             })
+            .state('indicators.create.main', {
+                url:'/main',
+                templateUrl: "modules/indicators/client/views/indicator-form-main.client.view.html"
+            })
+            .state('indicators.create.activity', {
+                url:"/activity/:activityInd",
+                params:{
+                    activityInd:{
+                        value:'0',
+                        squash:false
+                    }
+                },
+                views:{
+                    "":{
+                        templateUrl:"modules/indicators/client/views/activity-details.client.view.html",
+                        controller: 'ActivitiesController',
+                        controllerAs: 'ac'
+                    },
+                     "input-details-main@indicators.form.activity.details":
+                    {
+                        templateUrl: "modules/indicators/client/views/input-details-main.client.view.html"
+                    },
+                }
+            })
+            .state('indicators.create.activity.form', {
+                url:"/form",
+                views:{
+                    "":{
+                        templateUrl:"modules/indicators/client/views/activity-form.client.view.html",
+                        controller:'ActivitiesController',
+                        controllerAs:'ac'
+                    },
+                     "input-details-main@indicators.form.activity.form":
+                    {
+                        templateUrl: "modules/indicators/client/views/input-details-main.client.view.html"
+                    }
+                }
+            })
+            .state('indicators.create.activity.input', {
+                url:"/input/:inputInd",
+                 params:{
+                    inputInd:{
+                        value:'0',
+                        squash:false
+                    }
+                },
+                controller:'InputsController',
+                controllerAs:'ic',
+                templateUrl: "modules/indicators/client/views/input-form.client.view.html"
+
+            })
+            .state('indicators.form.input.detail', {
+                url:"/details/:id",
+                templateUrl:"modules/indicators/client/views/input-details.client.view.html"
+            })
+            .state('indicators.form.input.form', {
+                url:"/form",
+                templateUrl:"modules/indicators/client/views/input-form.client.view.html"
+            })
+
             .state('indicators.edit',
             {
                 url: '/:indicatorId/edit',
-                templateUrl: 'modules/indicators/client/views/form-indicator.client.view.html',
-                controller: 'IndicatorsController',
-                controllerAs: 'vm',
+                 views:
+                {
+                    "":
+                    {
+                        templateUrl: 'modules/indicators/client/views/creator-main.client.view.html',
+                        controller: 'IndicatorsController',
+                        controllerAs: 'vm',
+                    },
+                    "side@indicators.edit":
+                    {
+                        templateUrl: "modules/indicators/client/views/creator-side.client.view.html"
+                    },
+                },
                 resolve:
                 {
-                    indicatorResolve: getIndicator
+                    indicatorResolve: getIndicator,
+                    indicatorId: getIndicatorId
                 },
                 data:
                 {
@@ -90,6 +142,61 @@
                     pageTitle: 'Edit Indicator {{ indicatorResolve.name }}'
                 }
             })
+            .state('indicators.edit.main', {
+                url:'/main',
+                templateUrl: "modules/indicators/client/views/indicator-form-main.client.view.html"
+            })
+
+            .state('indicators.edit.activity', {
+                url:"/activity/:activityInd",
+                params:{
+                    activityInd:{
+                        value:'0',
+                        squash:false
+                    }
+                },
+                views:{
+                    "":{
+                        templateUrl:"modules/indicators/client/views/activity-details.client.view.html",
+                        controller: 'ActivitiesController',
+                        controllerAs: 'ac'
+                    },
+                    "input-details-main@indicators.edit.activity":
+                    {
+                        templateUrl: "modules/indicators/client/views/input-details-main.client.view.html"
+                    },
+                },
+                 onEnter:function(){console.log("entering activity ")}
+            })
+            .state('indicators.edit.activity.form', {
+                url:"/form",
+                views:{
+                    "":{
+                        templateUrl:"modules/indicators/client/views/activity-form.client.view.html",
+                        controller:'ActivitiesController',
+                        controllerAs:'ac'
+                    },
+                    "input-details-main@indicators.edit.activity.form":
+                    {
+                        templateUrl: "modules/indicators/client/views/input-details-main.client.view.html"
+                    }
+                },
+                onEnter:function(){console.log("entering activity form")}
+            })
+            .state('indicators.edit.activity.input', {
+                url:"/input/:inputInd",
+                params:{
+                    inputInd:{
+                        value:'0',
+                        squash:false
+                    },
+                },
+                controller:'InputsController',
+                controllerAs:'ic',
+                templateUrl: "modules/indicators/client/views/input-form.client.view.html"
+
+            })
+
             .state('indicators.view',
             {
                 url: '/:indicatorId',
@@ -98,7 +205,8 @@
                 controllerAs: 'vm',
                 resolve:
                 {
-                    indicatorResolve: getIndicator
+                    indicatorResolve: getIndicator,
+                    indicatorId: getIndicatorId
                 },
                 data:
                 {
@@ -106,11 +214,85 @@
                 }
             });
     }
-
+    getIndicatorId.$inject = ['$stateParams'];
+    function getIndicatorId($stateParams){
+        if($stateParams.indicatorId)
+            return $stateParams.indicatorId;
+        else return {};
+    }
     getIndicator.$inject = ['$stateParams', 'IndicatorsService'];
 
     function getIndicator($stateParams, IndicatorsService)
     {
+        var indicators =
+        [
+            {
+            _id: "100",
+            title:"test 1 ",
+            description: "test 1 desc",
+            activities: [
+            {
+                title: "test 1 act 1",
+                description : "test 1 act 1 desc",
+                inputs: [{
+                    text: "test 1 act 1 input 1",
+                    type:"yesno",
+                },{
+                    text: "another test 1 act1 input 2",
+                    type:"freetext"
+                }]
+            },
+            {
+                title: "test 1 act 2",
+                description : "test 1 act 2 desc",
+                inputs: [{
+                    text: "test 1 act 2 input 1",
+                    type:"yesno",
+                },{
+                    text: "another test 1 act 2 input 2",
+                    type:"freetext"
+                }]
+            }
+            ]
+            },
+            {
+                _id: "101",
+                title:"test 2 ",
+                description: "test 2 desc",
+                activities: [
+                {
+                    title: "test 2 act 1",
+                    description : "test 2 act 1 desc",
+                    inputs: [{
+                        text: "test 2 act 1 input 1",
+                        type:"yesno",
+                    },{
+                        text: "another test 2 act1 input 2",
+                        type:"freetext"
+                    }]
+                },
+                {
+                    title: "test 2 act 2",
+                    description : "test 2 act 2 desc",
+                    inputs: [{
+                        text: "test 2 act 2 input 1",
+                        type:"yesno",
+                    },{
+                        text: "another test 2 act 2 input 2",
+                        type:"freetext"
+                    }]
+                }
+                ]
+            }
+        ];
+        for(var i = 0; i < indicators.length ; i++){
+            if(indicators[i]._id == $stateParams.indicatorId)
+            {
+                return indicators[i]
+            }
+        }
+        return {error:"shit happens"};
+
         return IndicatorsService.get(
         {
             indicatorId: $stateParams.indicatorId
--- modules/indicators/client/controllers/indicators.client.controller.js
+++ modules/indicators/client/controllers/indicators.client.controller.js
@@ -6,134 +6,46 @@
         .module('indicators')
         .controller('IndicatorsController', IndicatorsController);
 
-    IndicatorsController.$inject = ['$scope', '$state', 'indicatorResolve', '$window', 'Authentication', 'IndicatorsCreateService', 'ReqsTempService'];
+    IndicatorsController.$inject = ['$scope', '$state', '$stateParams', 'indicatorResolve','indicatorId', '$window', 'Authentication', 'IndicatorsCreateService', 'ReqsTempService'];
 
-    function IndicatorsController($scope, $state, indicator, $window, Authentication, IndicatorsCreateService, ReqsTempService)
+    function IndicatorsController($scope, $state, $stateParams, indicator,indicatorId, $window, Authentication, IndicatorsCreateService, ReqsTempService)
     {
         var vm = this;
+        //What we are working with
         vm.indicator = indicator;
+        //quick n dirty until we change model to final form
         if(!vm.indicator.requirements)vm.indicator.requirements=[];
+        if(!vm.indicator.activities)vm.indicator.activities=[];
+
         vm.authentication = Authentication;
         vm.error = null;
         vm.form = {};
         vm.remove = remove;
         vm.save = save;
-        vm.addActivity = addActivity;
-        vm.removeActivity = removeActivity;
-        vm.showInput = showInput;
-        vm.goToInputForm = goToInputForm;
-        vm.saveInput = saveInput;
-        vm.addAnswerAlternative = addAnswerAlternative;
-        vm.activity = {};
-        vm.input = {};
-        vm.Requirements = [
-            {
-                id: 1123,
-                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime sequi in modi amet ducimus, ipsam rerum saepe ut eligendi. Amet ipsum nulla aliquid autem ea iusto id qui doloribus voluptatum.",
-                category: "A",
-                standard: "STD 101"
-            },
-            {
-                id: 1124,
-                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime sequi in modi amet ducimus, ipsam rerum saepe ut eligendi. Amet ipsum nulla aliquid autem ea iusto id qui doloribus voluptatum.",
-                category: "B",
-                standard: "STD 102"
-            },
-            {
-                id: 1125,
-                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime sequi in modi amet ducimus, ipsam rerum saepe ut eligendi. Amet ipsum nulla aliquid autem ea iusto id qui doloribus voluptatum.",
-                category: "A",
-                standard: "STD 101"
-            },
-            {
-                id: 1126,
-                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime sequi in modi amet ducimus, ipsam rerum saepe ut eligendi. Amet ipsum nulla aliquid autem ea iusto id qui doloribus voluptatum.",
-                category: "C",
-                standard: "STD 101"
-            },
-            {
-                id: 1127,
-                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime sequi in modi amet ducimus, ipsam rerum saepe ut eligendi. Amet ipsum nulla aliquid autem ea iusto id qui doloribus voluptatum.",
-                category: "C",
-                standard: "STD 102"
-            }
-        ];
+        vm.addActivity  = addActivity;
+        vm.indicatorId=indicatorId;
+
+        vm.editIndicator = function(id){
+            $state.go('indicators.edit.main', {indicatorId: indicatorId})
+        }
+
+        vm.Requirements = [];
+
         vm.addRequirement=addRequirement;
         vm.removeRequirement=removeRequirement;
 
         vm.createService = IndicatorsCreateService;
-        vm.details = vm.createService.details;
-
+        vm.createService.indicator = vm.indicator;
         vm.listSelected;
-        vm.indicator.title = "1.2.3 - Indikator Taktil";
-        vm.indicator.description = "";
-        vm.indicator.activities =
-        [
-            {
-                title: "Test Act 1",
-                description: "Test act 1 description.",
-                inputs: [
-                {
-                    text: "Is it blue?",
-                    type: "yesno"
-                },
-                {
-                    text: "What is 2+2?",
-                    type: "numeric"
-                }]
-            },
-            {
-                title: "Test Act 2",
-                description: "Test act 2 description.",
-                inputs: [
-                {
-                    text: "Is it green?",
-                    type: "yesno"
-                },
-                {
-                    text: "What is 3+3?",
-                    type: "numeric"
-                }]
+        function addActivity(){
+            var act={
+                title:'Aktivtet uten navn',
+                inputs:[],
+                beskrivelse: '',
             }
-        ]
-
-        vm.inputTypes =
-        [
-            {
-                name: "yesno",
-                name_no: 'Ja / Nei',
-                url: "modules/indicators/client/views/inputs/yesNo.html"
-            },
-            {
-                name: "radio",
-                name_no: 'Radioknapper',
-                url: "modules/indicators/client/views/inputs/radio.html"
-            },
-            {
-                name: "checkbox",
-                name_no: 'Checkbox',
-                url: "modules/indicators/client/views/inputs/checkbox.html"
-            },
-            {
-                name: "freetext",
-                name_no: 'Fritekst',
-                url: "modules/indicators/client/views/inputs/freetext.html"
-            },
-            {
-                name: "numeric",
-                name_no: 'Numerisk',
-                url: "modules/indicators/client/views/inputs/numeric.html"
-            },
-            {
-                name: "picture",
-                name_no: 'Bilde',
-                url: "modules/indicators/client/views/inputs/picture.html"
-            }
-        ];
-
-
-
-
+            vm.indicator.activities.push(act);
+            $state.go('^.activity.form', {activityInd: vm.indicator.activities.length-1});
+        }
         function addRequirement(req){
             if(!vm.indicator.requirements)vm.indicator.requirements = [];
             vm.indicator.requirements.push(req);
@@ -144,84 +56,7 @@
             else
                 vm.indicator.requirements.splice(vm.indicator.requirements.indexOf(req), 1);
         }
-        function addAnswerAlternative()
-        {
-            console.log(vm.input.options.alternative)
-            if (!vm.input.alternatives) vm.input.alternatives = [];
-            vm.input.alternatives.push(vm.input.options.alternative);
-            vm.input.options.alternative = {};
-        }
 
-        function saveInput(input)
-        {
-            if (!input.options.isNew)
-            {
-                // input.options.activity.inputs.push(input);
-                console.log(input.options)
-            }
-            else
-            {
-                input.options.activity.inputs.push(input);
-            }
-            console.log("printing input: ")
-            console.log(input)
-        }
-
-        function showInput(type)
-        {
-            vm.inputShow = true;
-            console.log(type)
-            vm.input.options.type = type;
-            vm.input.type = type.name;
-        }
-
-        function editInput(input)
-        {
-            vm.createService.setMainView(vm.createService.views.currentView,
-            {
-                details: vm.createService.details
-            });
-            vm.createService.goToView('input-form', input,
-            {
-                header: "Endre registrering"
-            });
-            vm.input = input;
-            vm.input.options = {
-                activity: vm.createService.details
-            };
-            for (var i = 0; i < vm.inputTypes.length; i++)
-            {
-                if (vm.inputTypes[i].name === input.type)
-                {
-                    showInput(vm.inputTypes[i]);
-                }
-            }
-        }
-
-        function goToInputForm(activity, input, options)
-        {
-            if (options && options.edit && options.edit == true)
-            {
-                editInput(input);
-            }
-            else
-            {
-                vm.createService.setMainView(vm.createService.views.currentView,
-                {
-                    details: activity
-                });
-                vm.createService.goToView('input-form', input,
-                {
-                    header: "Ny registrering"
-                });
-                vm.input = {};
-                vm.input.options = {
-                    isNew: true,
-                    activity: activity
-                };
-                showInput(vm.inputTypes[0])
-            }
-        }
         // Remove existing indicator
         function remove()
         {
@@ -229,24 +64,6 @@
             {
                 vm.indicator.$remove($state.go('indicators.list'));
             }
-        }
-
-        function addActivity()
-        {
-            vm.createService.details = {
-                title: "Aktivitet uten navn",
-                description: "Ingen Beskrivelse",
-                krav: [],
-                inputs: []
-            }
-            vm.indicator.activities.push(vm.createService.details);
-            vm.createService.goToView('form-activity', vm.createService.details)
-        }
-
-        function removeActivity(obj)
-        {
-            var index = vm.indicator.activities.indexOf(obj);
-            vm.indicator.activities.splice(index, 1);
         }
         // Save indicator
         function save(isValid)
