b92953c0af2feaf38bcbef8ac85a8047d828005b
rogerww <trzy.akordy@gmail.com>
2016-07-12 23:11:55 +0200

    small fixes to inputs

--- modules/indicators/client/controllers/activities.client.controller.js
+++ modules/indicators/client/controllers/activities.client.controller.js
@@ -6,18 +6,22 @@
         .module('indicators')
         .controller('ActivitiesController', ActivitiesController);
 
-    ActivitiesController.$inject = ['$scope', '$state', '$stateParams','indicatorId', 'Authentication', 'IndicatorsCreateService'];
+    ActivitiesController.$inject = ['$scope', '$state', '$stateParams', 'indicatorId', 'Authentication', 'IndicatorsCreateService'];
 
     function ActivitiesController($scope, $state, $stateParams, indicatorId, Authentication, IndicatorsCreateService)
     {
         var vm = this;
 
-        try{
+        try
+        {
             vm.activity = IndicatorsCreateService.indicator.activities[$stateParams.activityInd];
             vm.activity.id = $stateParams.activityInd;
-        }catch(e){
+        }
+        catch (e)
+        {
+            console.log("Something went wrong while retrieving activity")
             console.log(e);
-            if($state.current.name.indexOf('create')>=0)$state.go('indicators.create.main')
+            if ($state.current.name.indexOf('create') >= 0) $state.go('indicators.create.main')
             else $state.go(".^.^.main");
         }
         //in case no saving will take place
@@ -25,61 +29,90 @@
 
         vm.removeInput = removeInput;
         vm.newInput = newInput;
-        vm.indicatorId=indicatorId;
+        vm.indicatorId = indicatorId;
         vm.cancel = cancel;
         vm.save = save;
         vm.editInput = editInput;
         vm.localRequirements = getLocalRequirements();
-        function getLocalRequirements(){
+
+        function getLocalRequirements()
+        {
             var local = [];
-            for(var i = 0; i < vm.activity.inputs.length; i++){
-                for(var j = 0; j < vm.activity.inputs[i].requirements.length; j++){
-                    if(local.indexOf( vm.activity.inputs[i].requirements[j])>=0){
+            for (var i = 0; i < vm.activity.inputs.length; i++)
+            {
+                for (var j = 0; j < vm.activity.inputs[i].requirements.length; j++)
+                {
+                    if (local.indexOf(vm.activity.inputs[i].requirements[j]) >= 0)
+                    {
                         console.log("dup");
                     }
-                    else{
-                        local.push( angular.copy(vm.activity.inputs[i].requirements[j]));
-                        if(!local[local.length-1].inputInd){
-                            local[local.length-1].inputInd = [];
+                    else
+                    {
+                        local.push(angular.copy(vm.activity.inputs[i].requirements[j]));
+                        if (!local[local.length - 1].inputInd)
+                        {
+                            local[local.length - 1].inputInd = [];
                         }
-                        local[local.length-1].inputInd.push(i+1);
+                        local[local.length - 1].inputInd.push(i + 1);
                     }
                 }
             }
             console.log(local)
             return local;
         }
-        function editInput(index){
+
+        function editInput(index)
+        {
             console.log("go to input edit state")
-            if($state.current.name.indexOf('form')>=0){
+            if ($state.current.name.indexOf('form') >= 0)
+            {
                 console.log("we are in form")
-                 $state.go('.^.input', {inputInd: index.toString()});
+                $state.go('.^.input',
+                {
+                    inputInd: index.toString()
+                });
             }
-            else{
+            else
+            {
                 console.log("we are not in form")
-                $state.go('.input', {inputInd: index.toString()})
+                $state.go('.input',
+                {
+                    inputInd: index.toString()
+                })
             }
         }
-        function removeInput(index){
+
+        function removeInput(index)
+        {
             vm.activity.inputs.splice(index, 1);
         }
-        function newInput(){
+
+        function newInput()
+        {
             var inp = {
-                text:'',
-                type:'',
-                alternatives:[],
-                requirements:[],
-                options:[],
+                text: '',
+                type: '',
+                mandytory: true,
+                alternatives: [],
+                requirements: [],
+                options: [],
             };
             vm.activity.inputs.push(inp);
-            editInput(vm.activity.inputs.length-1)
+            editInput(vm.activity.inputs.length - 1)
         }
-        function cancel(){
-           IndicatorsCreateService.indicator.activities[$stateParams.activityInd] = oldActivity;
-           $state.go('.^');
+
+        function cancel()
+        {
+            IndicatorsCreateService.indicator.activities[$stateParams.activityInd] = oldActivity;
+            $state.go('.^');
         }
-        function save(){
-            $scope.vm.save(true, {remainInThisState:true});
+
+        function save()
+        {
+            $scope.vm.save(true,
+            {
+                remainInThisState: true
+            });
         }
     }
 }());
--- modules/indicators/client/controllers/inputs.client.controller.js
+++ modules/indicators/client/controllers/inputs.client.controller.js
@@ -1,4 +1,5 @@
-(function() {
+(function()
+{
     'use strict';
 
     angular
@@ -7,13 +8,16 @@
 
     InputsController.$inject = ['$state', '$scope', '$stateParams', 'Authentication', 'IndicatorsCreateService', 'RequirementsService'];
 
-    function InputsController($state, $scope, $stateParams, Authentication, IndicatorsCreateService, RequirementsService) {
+    function InputsController($state, $scope, $stateParams, Authentication, IndicatorsCreateService, RequirementsService)
+    {
         var vm = this;
 
-        try {
+        try
+        {
             vm.input = IndicatorsCreateService.indicator.activities[$stateParams.activityInd].inputs[$stateParams.inputInd];
         }
-        catch(e){
+        catch (e)
+        {
             console.log(e);
             goBack();
         }
@@ -26,50 +30,84 @@
         vm.reqSearch = [reqsGlobal, reqsLocal];
         vm.reqSearchInd = 1;
         vm.addRequirement = addRequirement;
-        function addRequirement(req){
-            if(vm.input.requirements.indexOf(req)>=0){
+        vm.addAlternative = addAlternative;
+
+        function addRequirement(req)
+        {
+            if (vm.input.requirements.indexOf(req) >= 0)
+            {
                 console.log("duplicate! ");
                 return;
             }
             vm.input.requirements.push(req);
         }
-
+        vm.alternative = {
+            text: '',
+            isCorrect: null,
+            options:
+            {}
+        }
         vm.inputTypes =
-            [{
-                name: "yesno",
-                name_no: 'Ja / Nei',
-                url: "modules/indicators/client/views/inputs/yesNo.html"
-            }, {
-                name: "radio",
-                name_no: 'Radioknapper',
-                url: "modules/indicators/client/views/inputs/radio.html"
-            }, {
-                name: "checkbox",
-                name_no: 'Checkbox',
-                url: "modules/indicators/client/views/inputs/checkbox.html"
-            }, {
-                name: "freetext",
-                name_no: 'Fritekst',
-                url: "modules/indicators/client/views/inputs/freetext.html"
-            }, {
-                name: "numeric",
-                name_no: 'Numerisk',
-                url: "modules/indicators/client/views/inputs/numeric.html"
-            }, {
-                name: "picture",
-                name_no: 'Bilde',
-                url: "modules/indicators/client/views/inputs/picture.html"
-            }];
+            [
+        {
+            name: "yesno",
+            name_no: 'Ja / Nei',
+            url: "modules/indicators/client/views/inputs/yesNo.html"
+        },
+        {
+            name: "radio",
+            name_no: 'Radioknapper',
+            url: "modules/indicators/client/views/inputs/radio.html"
+        },
+        {
+            name: "checkbox",
+            name_no: 'Checkbox',
+            url: "modules/indicators/client/views/inputs/checkbox.html"
+        },
+        {
+            name: "freetext",
+            name_no: 'Fritekst',
+            url: "modules/indicators/client/views/inputs/freetext.html"
+        },
+        {
+            name: "numeric",
+            name_no: 'Numerisk',
+            url: "modules/indicators/client/views/inputs/numeric.html"
+        },
+        {
+            name: "picture",
+            name_no: 'Bilde',
+            url: "modules/indicators/client/views/inputs/picture.html"
+        }];
 
-        function goBack() {
+        function addAlternative()
+        {
+            vm.input.alternatives.push(vm.alternative);
+            vm.alternative = {
+                text: '',
+                isCorrect: null,
+                options:
+                {}
+            };
+        }
+
+        function goBack()
+        {
             $state.go('.^')
         }
-        function cancel(){
+
+        function cancel()
+        {
             IndicatorsCreateService.indicator.activities[$stateParams.activityInd].inputs[$stateParams.inputInd] = oldInput;
-              goBack();
+            goBack();
         }
-        function save(){
-            $scope.ac.save(true, {remainInThisState:true});
+
+        function save()
+        {
+            $scope.ac.save(true,
+            {
+                remainInThisState: true
+            });
             goBack();
         }
 
